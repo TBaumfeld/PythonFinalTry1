41e1bd010004be9f3d13be9c01981ab7cc4ebfb2
TBaumfeld <trbaumfeld@gmail.com>
2024-12-12 20:35:29 -0500

    Update streamlitattempt2.py

--- streamlitattempt2.py
+++ streamlitattempt2.py
@@ -1,3 +1,8 @@
+!pip install pandas
+!pip install streamlit
+import pandas as pd
+import streamlit as st
+
 st.header("Skyscrapers rank vs. Height")
 df = pd.read_csv('skyscrapers(in).csv', index_col = 0)
 df[["statistics.rank" , "statistics.height"]].plot.line(x = "statistics.rank" , y = "statistics.height")
