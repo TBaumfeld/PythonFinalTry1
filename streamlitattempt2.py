51600096a3ca8432b5736407fbfa7baa36c761bd
TBaumfeld <trbaumfeld@gmail.com>
2024-12-12 20:34:03 -0500

    Create streamlitattempt2.py

---
+++ streamlitattempt2.py
@@ -0,0 +1,3 @@
+st.header("Skyscrapers rank vs. Height")
+df = pd.read_csv('skyscrapers(in).csv', index_col = 0)
+df[["statistics.rank" , "statistics.height"]].plot.line(x = "statistics.rank" , y = "statistics.height")
